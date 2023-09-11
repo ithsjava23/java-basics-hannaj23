6f01f0f41c187db6b33c11c062b280ad7ffa3c6a
Hanna Johansson <hanna.johansson@iths.se>
2023-09-11 09:28:59 +0200

    Meny utskrift

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner sc = new Scanner(System.in);
+
+        Meny();
+
+
     }
+    public static void Meny() {
+        System.out.print("Elpriser\n"
+                        +"========\n"
+                        +"1. Inmatning\n"
+                        +"2. Min, Max och Medel\n"
+                        +"3. Sortera\n"
+                        +"4. Bästa Laddningstid (4h)\n"
+                        +"e. Avsluta\n");
+        System.out.print("Skriv in ditt val: ");
+
+    }
+
+
+
+
+
+
+
+
+
 }
