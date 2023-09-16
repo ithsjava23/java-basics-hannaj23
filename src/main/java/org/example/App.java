f1cdc01625dfcfa3c91f8a3683b8113537a5b0ce
Hanna Johansson <hanna.johansson@iths.se>
2023-09-16 16:58:08 +0200

    alla menyvalmetoder gjorda

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,6 @@ package org.example;
 
 import java.util.*;
 import java.util.Arrays;
-import java.util.Dictionary;
 public class App {
 
     public static void main(String[] args) {
@@ -13,42 +12,35 @@ public class App {
             int[] elPriserInput = new int[24];
             String[] tidsintervall = {"00-01", "01-02", "02-03", "03-04", "04-05", "05-06", "06-07", "07-08", "08-09", "09-10", "10-11", "11-12",
                                   "12-13", "13-14", "14-15", "15-16", "16-17", "17-18", "18-19", "19-20", "20-21", "21-22", "22-23", "23-24"};
+
             String choice;
             do {
                 PrintMenu();
                 choice = scInput.nextLine();
 
                 switch (choice) {
-                    case "1":
-                        MenyVal1(elPriserInput, tidsintervall, scInput);
-                        break;
-                    case "2":
-                        MenyVal2(elPriserInput, tidsintervall);
-                        break;
-                    case "3":
-                        MenyVal3(elPriserInput, tidsintervall);
-                        break;
-                    case "4":
-                        MenyVal4(elPriserInput);
-                        break;
-                    case "e","E":
-                        break;
-                    default:
-
+                    case "1" -> MenyVal1(elPriserInput, tidsintervall, scInput);
+                    case "2" -> MenyVal2(elPriserInput, tidsintervall);
+                    case "3" -> MenyVal3(elPriserInput, tidsintervall);
+                    case "4" -> MenyVal4(elPriserInput);
+                    case "e", "E" -> {}
+                    default -> {
+                    }
                 }
             } while (!choice.equalsIgnoreCase("e"));
-
-           // System.exit(0);
     }
 
         public static void PrintMenu() {
-            System.out.print("\nElpriser\n"
-                    +"========\n"
-                    +"1. Inmatning\n"
-                    +"2. Min, Max och Medel\n"
-                    +"3. Sortera\n"
-                    +"4. Bästa Laddningstid (4h)\n"
-                    +"e. Avsluta\n");
+            System.out.print("""
+
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """);
 
         }
         public static void MenyVal1(int[] elPriserInput, String[] tidsintervall, Scanner scInput){
@@ -58,7 +50,6 @@ public class App {
                 System.out.print(tidsintervall[i] + ": ");
                 elPriserInput[i] = Integer.parseInt(scInput.nextLine());
             }
-
         }
 
         public static void MenyVal2(int[] elPriserInput, String[] tidsintervall) {
@@ -71,7 +62,6 @@ public class App {
                     indexLowPrice = i;
                 }
             }
-            System.out.print("Lägsta pris: " + tidsintervall[indexLowPrice] + ", " + minPrice + " öre/kWh\n");
 
 
             int bigPrice = elPriserInput[0];
@@ -82,19 +72,18 @@ public class App {
                     indexBigPrice = i;
                 }
             }
-            System.out.print("Högsta pris: " + tidsintervall[indexBigPrice] + ", " + bigPrice + " öre/kWh\n");
-
 
             int sumPrices = 0;
             float averagePrice;
-            for (int i = 0; i < elPriserInput.length; i++) {
-                sumPrices += elPriserInput[i];
+            for (int j : elPriserInput) {
+                sumPrices += j;
             }
             averagePrice = sumPrices / 24.0f;
+
+            System.out.print("Lägsta pris: " + tidsintervall[indexLowPrice] + ", " + minPrice + " öre/kWh\n");
+            System.out.print("Högsta pris: " + tidsintervall[indexBigPrice] + ", " + bigPrice + " öre/kWh\n");
             System.out.printf("Medelpris: %.2f öre/kWh\n", averagePrice);
             }
-
-
 
 
         public static void MenyVal3(int[] prices, String[] tidsintervall){
@@ -105,14 +94,6 @@ public class App {
             }
             Arrays.sort(index, Comparator.comparingInt(i -> -prices[i]));
 
-
-            /*Arrays.sort(prices);
-            int n = prices.length;
-            for (int i = 0; i < prices.length/2; i++) {
-                int temp = prices[i];
-                prices[i] = prices[prices.length-1-i];
-                prices[prices.length-1-i] = temp;
-            }*/
             for (int i = 0; i < prices.length; i++) {
                 System.out.print(tidsintervall[index[i]] + " " + prices[index[i]] + " öre\n");
             }
